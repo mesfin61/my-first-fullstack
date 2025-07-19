d8a8a30d895424e1491727fa3d5930a1bb4ea323
Mesfin Ayele <mesfinayele499@gmail.com>
2025-07-19 17:08:52 +0300

    adding header and some packages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,45 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LandingPage from './components/LandingPage';
-import Home from './components/Home'
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import Home from "./components/Home";
+import Courses from "./components/Courses";
+import Header from "./components/Header";
+import "./App.css";
+import User from "./components/User";
+
+// Layout with Header
+const Layout = ({ children }) => (
+  <>
+    <Header />
+    {children}
+  </>
+);
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </Router>
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/user" element={<User />} />
+        <Route
+          path="/home"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/courses"
+          element={
+            <Layout>
+              <Courses />
+            </Layout>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { Link } from "react-router";
+import "../assets/styles/header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="logo">CourseEra</h1>
+      <div className="nav-link">
+        <Link to="/home">home</Link>
+        <Link to="/courses">courses</Link>
+      </div>
+      <div className="profile">
+        <Link to="/profile">
+          <AccountCircleIcon className="pro" />
+        </Link>
+      </div>
+    </header>
+  );
+}
