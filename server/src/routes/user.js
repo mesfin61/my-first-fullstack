f8bc8e935f58f33524a5ae8cc378fbb7943c47e9
Mesfin Ayele <mesfinayele499@gmail.com>
2025-08-04 14:02:25 +0300

    fixing deleting account problem

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -5,11 +5,13 @@ const profile = require("../utils/profile");
 const account = require("../utils/accountmanagement");
 const password = require("../utils/passwordmanagement");
 const authenticateToken = require("../../middleware/authMiddleware");
+const deleteAccount = require("../utils/delete");
 
 router.post("/api/auth/signup", register);
 router.post("/api/auth/login", loginLimitter, login);
 router.get("/profile", authenticateToken, profile);
 router.put("/profile/update", authenticateToken, account);
 router.put("/profile/update/password", authenticateToken, password);
+router.delete("/profile/delete", authenticateToken, deleteAccount);
 
 module.exports = router;
---
+++ server/src/utils/delete.js
@@ -0,0 +1,27 @@
+const { poolPromise, mssql } = require("../config/db");
+const deleteAccount = async (req, res) => {
+  try {
+    const pool = await poolPromise;
+    const data = await pool
+      .request()
+      .input("userId", mssql.Int, req.user.id)
+      .query(`SELECT * FROM userInfo WHERE userId = @userId`);
+
+    if (data.recordset.length === 0) {
+      return res.status(401).json({ message: "user account not found" });
+    }
+
+    await pool
+      .request()
+      .input("userId", mssql.Int, req.user.id)
+      .query(`DELETE FROM userInfo WHERE userId = userId`);
+
+    return res.status(200).json({ message: "Account deleted successfully" });
+  } catch (err) {
+    console.error("Error deleting account:", err);
+
+    return res.status(500).json({ message: "internal server error" });
+  }
+};
+
+module.exports = deleteAccount;
