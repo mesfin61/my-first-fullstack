9734da51a882189e49c988dcfe3fc1363191b8f2
Mesfin Ayele <mesfinayele499@gmail.com>
2025-08-17 11:53:07 +0300

    trying to fix problem

--- server/src/index.js
+++ server/src/index.js
@@ -5,17 +5,13 @@ require("dotenv").config();
 
 const app = express();
 app.set("trust proxy", 1);
-app.use(
-  cors({
-    origin: "https://myway-learning.vercel.app",
-    credentials: true,
-  })
-);
+
+app.use(cors());
+
 app.use(express.json());
 app.use(user);
 
-const PORT = process.env.PORT;
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);
 });
