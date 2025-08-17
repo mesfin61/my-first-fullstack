21d65743f98fed97aa516ec2ee369c870ca2aa33
Mesfin Ayele <mesfinayele499@gmail.com>
2025-08-17 12:12:42 +0300

    final update for backend

--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,12 @@ require("dotenv").config();
 const app = express();
 app.set("trust proxy", 1);
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://myway-learning.vercel.app",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(user);
