1538fee9b8896edd2d5b7b31622f5c05849a93f5
Mesfin Ayele <mesfinayele499@gmail.com>
2025-08-17 10:53:43 +0300

    fixing bugs

--- server/src/index.js
+++ server/src/index.js
@@ -5,12 +5,7 @@ require("dotenv").config();
 
 const app = express();
 app.set("trust proxy", 1);
-app.use(
-  cors({
-    origin: "https://my-way-backend.up.railway.app",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(user);
 
