aaeee5aa85a68974edaa5ca24a256affaa09fd00
Mesfin Ayele <mesfinayele499@gmail.com>
2025-08-17 11:19:19 +0300

    preparing for deployment

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,12 @@ require("dotenv").config();
 
 const app = express();
 app.set("trust proxy", 1);
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://myway-learning.vercel.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(user);
 
